f1e1d8a0ebb7ef68e5cad4df1b99fa2a1618cbf7
Nazar Ignatenko <nazar.ignatenkoru@gmail.com>
2025-06-07 23:05:38 +0300

    feat: layout updated

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,10 +12,10 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Учетные записи пользователей
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>by Nazar Ignatenko</div>
       </q-toolbar>
     </q-header>
 
@@ -28,7 +28,7 @@
         <q-item-label
           header
         >
-          Essential Links
+          Меню
         </q-item-label>
 
         <EssentialLink
